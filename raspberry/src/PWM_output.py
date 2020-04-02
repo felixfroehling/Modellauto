035185b5827eb1feebd85384edc6d70a52c35738
felixfroehling <63063096+felixfroehling@users.noreply.github.com>
2020-04-02 20:45:02 +0100

    Update PWM_output.py

--- raspberry/src/PWM_output.py
+++ raspberry/src/PWM_output.py
@@ -1,56 +1,54 @@
 #!/usr/bin/python
 # coding=utf-8
 
+# With this Node the Servomotor is controlled by a simple PWM Signal
+# The Servomotor is used for the steering
+# Credits to Felix Fröhling
+
+
+# Import for ROS
 import rospy
 from std_msgs.msg import String, Int32
 
+# Import for the PWM Signal generation
 import RPi.GPIO as GPIO
 import time
 
 
 
 def callback(data):
-    #print("got data")
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    # Callback 0
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)  # Debug information
     buf = float(data.data)
     print(buf)
     #print(type(buf))
     changeSteering(buf)
-    #data = buf.decode('utf-8')
-    #dataMessage = data.split(' ', 1)
-    #state = dataMessage[0]
-    #value = dataMessage[1]
     
     
 def changeSteering(value):
-    #Value is between -50 and 50
+    # Value is between -50 and 50
+    # +50 is full right, -50 is full left
     output_value = 5.5 * (value+50)/100 + 4.3
-    p.ChangeDutyCycle(output_value)
-    #value += 30    #ofset 0 - 100
-    #p.ChangeDutyCycle(value)
+    p.ChangeDutyCycle(output_value)   # Set PWM cicle
 
 
 rospy.init_node('Steering_Node', anonymous=True)
 rospy.Subscriber("steering", String, callback)
 
+# Initialization for GPIO 17 (PWM output Pin)
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
-p.start(7) # Initialisierung
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 as PWM output pin with 50Hz
+p.start(7) # Start PWM output
 try:
   while True:
-    #global state
-    ##if state == 'go':
-    #    p.ChangeDutyCycle(5)
-    #elif state == 'stop':
-    #    p.ChangeDutyCycle(12.5)
+    # p.changeDutyCycle(7.5)  #  1,5ms High means 90°. So steering is straight
     eingabe = input("Ihre Eingabe ")
-    if eingabe == quit:
+    if eingabe == "quit":
         break
-    #p.ChangeDutyCycle(7.5) #1,5ms 90°
-    #time.sleep(0.5)
+
 except: #KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
